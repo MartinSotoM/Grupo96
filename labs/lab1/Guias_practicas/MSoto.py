67a950e8e18279c4749c3750d222b2a2cfdbfa95
MartinSotoM <martin.soto.m@mail.pucv.cl>
2026-03-11 11:43:28 -0300

    Ejercicios de práctica completos con intrfaz de menú

--- labs/lab1/Guias_practicas/MSoto.py
+++ labs/lab1/Guias_practicas/MSoto.py
@@ -21,15 +21,66 @@ def ejercicio_1():
 def ejercicio_2():
     print("\n--- Ejecutando Ejercicio 2 ---")
 
-    fact = int(input("Ingresa el número a calcular: "))
+    numero = int(input("Ingresa el número a calcular: "))
 
-    if fact <= 0:
+    if numero <= 0:
         print("Su número debe ser entero positivo.")
     else:
-        c = 1 #Resultado Temporal
-        for j in range(1, fact + 1):
-            c = c*j #Mult factorial
-            print(f"Factorial de {j} es {c}")
+        x = 1 #Resultado Temporal
+        for i in range(1, numero + 1):
+            x = x*i #Mult factorial
+            print(f"Factorial de {i} es {x}")
+
+
+def ejercicio_3():
+    print("\n--- Ejecutando Ejercicio 3 ---")
+
+    numero = int(input("Ingresa el número a calcular: "))
+
+    while numero <= 0:
+        print("Su número debe ser entero positivo.")
+        numero = int(input("Ingrese un número válido: "))
+
+        x = 1 #Resultado Temporal
+        for i in range(1, numero + 1):
+            x = x*i #Mult factorial
+            print(f"Factorial de {i} es {x}")
+
+
+def ejercicio_4():
+    print("\n--- Ejecutando Ejercicio 4 ---")
+
+    conexion = int(input("Seleccione el tipo de conexión (1 = serie, 2 = paralelo): "))
+
+    if conexion != 1 and conexion != 2:
+        print("Error, no ingresaste un número válido.")
+    else:
+        cantidad = int(input("Ingrese la cantidad de resistencias: "))
+
+        if cantidad <= 0:
+            print("Error, el valor de cantidad debe ser un número entero positivo.")
+        else:
+            valores = []
+
+            for i in range(1, cantidad + 1):
+                r_actual = float(input(f"Ingrese valor de la resistencia {i}: "))
+
+                while r_actual <= 0:
+                    print("El valor de la resistencia debe ser mayor a 0")
+                    r_actual = float(input(f"Ingrese un valor válido de la resistencia {i}: "))
+
+                valores.append(r_actual)
+
+            print(f"Valores de resistencia guardados: {valores}")
+
+    if conexion == 1:
+        resistencia_total = sum(valores)
+        print(f"La resistencia total en serie es: {resistencia_total} Ohms")
+    
+    else:
+        resis_paralelo = 1 / sum(1/r for r in valores)
+        print(f"La resistencia total en paralelos es: {resis_paralelo} Ohms")
+
 
 
 def menu():
@@ -37,6 +88,8 @@ def menu():
         print("\n--- MENÚ DE EJERCICIOS ---")
         print("1. Ejecutar Ejercicio 1")
         print("2. Ejecutar Ejercicio 2")
+        print("3. Ejecutar Ejercicio 3")
+        print("4. Ejecutar Ejercicio 4")
         print("0. Salir")
 
         opcion = input("Selecciona una opción: ")
@@ -45,6 +98,10 @@ def menu():
             ejercicio_1()
         elif opcion == "2":
             ejercicio_2()
+        elif opcion == "3":
+            ejercicio_3()
+        elif opcion == "4":
+            ejercicio_4()
         elif opcion == "0":
             print("Saliendo del programa...")
             break
