0f10f07765c41a85a19cb407422b6b4ec6e7e236
MartinSotoM <martin.soto.m@mail.pucv.cl>
2026-04-07 16:43:46 -0400

    Robot_data finalizado, pasamos a visualization

---
+++ tarea1_robot_beach/processing/cinematicas.py
@@ -0,0 +1,10 @@
+def calcular_movimiento(x, y, theta, v, omega, dt = 0.1):
+    return (x_nuevo, y_nuevo, theta_nuevo)
+
+
+def distancia_al_objetivo(x, y, x_meta, y_meta):
+    return
+
+
+def calcular_error_seguimiento(x_real, y_real, x_ideal, y_ideal):
+    return
