9c52d6cb7ac5d280b63a8ca9b4fa04967e1779f6
MartinSotoM <martin.soto.m@mail.pucv.cl>
2026-04-08 11:34:41 -0400

    Cinematicas y Metricas finalizadas

--- tarea1_robot_beach/processing/cinematicas.py
+++ tarea1_robot_beach/processing/cinematicas.py
@@ -1,10 +1,29 @@
+import numpy as np
+
+# Nueva pose
 def calcular_movimiento(x, y, theta, v, omega, dt = 0.1):
+    v_segura = np.clip(v, -0.8, 0.8)
+    omega_segura = np.clip(omega, -0.6, 0.6)
+    
+    x_nuevo = x + v_segura * np.cos(theta) * dt
+    y_nuevo = y + v_segura * np.sin(theta) * dt
+    theta_nuevo = theta + omega_segura * dt
+
     return (x_nuevo, y_nuevo, theta_nuevo)
 
-
+# Distancia Euclidiana
 def distancia_al_objetivo(x, y, x_meta, y_meta):
-    return
+    return float(np.sqrt((x_meta - x)**2 + (y_meta - y)**2))
 
-
+# Arreglos de Trayectoria
 def calcular_error_seguimiento(x_real, y_real, x_ideal, y_ideal):
-    return
+    largo_minimo = min(len(x_real), len(x_ideal))
+    
+    xr = x_real[:largo_minimo]
+    yr = y_real[:largo_minimo]
+    xi = x_ideal[:largo_minimo]
+    yi = y_ideal[:largo_minimo]
+    
+    errores = np.sqrt((xr - xi)**2 + (yr - yi)**2)
+    
+    return errores
--- tarea1_robot_beach/processing/metricas.py
+++ tarea1_robot_beach/processing/metricas.py
@@ -1,13 +1,33 @@
-def calcular_IAE():
+import numpy as np
 
-def calcular_ISE():
+# Cálculos
+def calcular_IAE(errores, dt):
+    return float(np.sum(np.abs(errores)) * dt)
 
-def calcular_ITAE():
+def calcular_ISE(errores, dt):
+    return float(np.sum(errores**2) * dt)
 
-def calcular_ITSE():
+def calcular_ITAE(errores, dt):
+    ti = np.arange(len(errores)) * dt
+    return float(np.sum(ti * np.abs(errores)) * dt)
 
-def calcular_todas_las_metricas():
-    return
+def calcular_ITSE(errores, dt):
+    ti = np.arange(len(errores)) * dt
+    return float(np.sum(ti * (errores**2)) * dt)
 
+# Función agrupadora
+def calcular_todas_las_metricas(errores, dt):
+    resultados = {
+        "ISE": round(calcular_ISE(errores, dt), 2),
+        "IAE": round(calcular_IAE(errores, dt), 2),
+        "ITSE": round(calcular_ITSE(errores, dt), 2),
+        "ITAE": round(calcular_ITAE(errores, dt), 2)
+    }
+    return resultados
+
+# Reducción del error
 def calcular_mejora(valor_ppo, valor_mask):
-    return
+    assert valor_ppo != 0, "El valor base (PPO) no puede ser cero, imposible calcular mejora."
+    
+    mejora = ((valor_ppo - valor_mask) / valor_ppo) * 100
+    return round(mejora, 2)
